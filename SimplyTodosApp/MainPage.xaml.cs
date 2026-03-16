7cda89f25ee555e22442288627c27aef2afc0f43
Kruze01 <you@example.com>
2026-03-16 16:48:35 +0800

    changed another drafted template for main page

--- SimplyTodosApp/MainPage.xaml.cs
+++ SimplyTodosApp/MainPage.xaml.cs
@@ -2,21 +2,41 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        string activated = "todos";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void TodosTabBtn_Clicked(object sender, EventArgs e)
+        private void todosBtn_Clicked(object sender, EventArgs e)
         {
+            if (activated != "todos")
+            {
+                activated = "todos";
+                todosBtn.BackgroundColor = Colors.Gray;
+                todosBtn.TextColor = Colors.White;
+                completionsBtn.BackgroundColor = Colors.GhostWhite;
+                completionsBtn.TextColor = Colors.Black;
+            }
 
         }
 
-        private void CompletionsTabBtn_Clicked(object sender, EventArgs e)
+        private void completionsBtn_Clicked(object sender, EventArgs e)
         {
+            if (activated != "completions")
+            {
+                activated = "completions";
+                todosBtn.BackgroundColor = Colors.GhostWhite;
+                todosBtn.TextColor = Colors.Black;
+                completionsBtn.BackgroundColor = Colors.Gray;
+                completionsBtn.TextColor = Colors.White;
+            }
+        }
 
+        private void addTaskBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlertAsync("Notification", "A new task has been added", "Okay");
         }
     }
 }
